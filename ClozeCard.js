9af5b56a5be3aa965494d1fd699364738b37655d
Xingxian Shou <Xingxian Shou>
2017-07-13 11:16:12 -0400

    second commit

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,11 +1,10 @@
 function ClozeCard(text, cloze) {
   this.cloze = cloze;
-  this.fullText = text;
+  this.text = text;
   this.part = text.replace(cloze, "...");
 }
 
 var card1 = new ClozeCard("George Washington is the first President of USA", "George Washington");
-card1.partial();
 console.log(card1);
 
 module.exports = ClozeCard;
---
+++ app.js
@@ -0,0 +1,6 @@
+var BasicCard = require("./Basic.js");
+var ClozeCard = require("./ClozeCard.js");
+
+var B1 = new BasicCard("First President of USA", "George Washington");
+var C1 = new ClozeCard();
+console.log(B1);
